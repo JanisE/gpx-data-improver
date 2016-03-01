dd1cdcbad9dcd2b1a1ae597262f382f451e05504
Jānis Elmeris <janise@inbox.lv>
2016-03-01 22:11:26 +0200

    - Support Geo Tracker exported GPX.

--- src/format.js
+++ src/format.js
@@ -1,11 +1,28 @@
 //var fJsToXml = new require('xml2js').Builder().buildObject;
 var xml2js = require('xml2js');
 var oJsToXml = new xml2js.Builder();
+var _ = require('underscore');
 
 
 function Format (oGpx)
 {
 	//console.log(util.inspect(oGpx, {depth: 12}));
+
+	// Remove needless "extensions" elements, e.g.:
+	// 		<trkpt ..><ele>..</ele><time>..</time><extensions><geotracker:meta c="220.3" s="0.5"/></extensions></trkpt>
+	_.each(oGpx.gpx.trk, function (oTrk)
+	{
+		// Remove also <extensinos> inside <trk>, or else Endomondo gives an error parsing file.
+		delete oTrk.extensions;
+
+		_.each(oTrk.trkseg, function (oTrkseg)
+		{
+			_.each(oTrkseg.trkpt, function (oTrkpt)
+			{
+				delete oTrkpt.extensions;
+			});
+		});
+	});
 
 	return oJsToXml.buildObject(oGpx)
 		// Format each <trkpt> into one line.
